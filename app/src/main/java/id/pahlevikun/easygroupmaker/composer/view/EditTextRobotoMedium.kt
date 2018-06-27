fc0da9a9ce2b1321dd8887b8d0aa6e2e35c7b78d
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-06-27 12:22:39 +0700

    remove espresso - 27/07/18 12:22

--- app/src/main/java/id/pahlevikun/easygroupmaker/composer/view/EditTextRobotoMedium.kt
+++ app/src/main/java/id/pahlevikun/easygroupmaker/composer/view/EditTextRobotoMedium.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Typeface
 import android.support.v7.widget.AppCompatEditText
 import android.util.AttributeSet
-import android.widget.EditText
 
 class EditTextRobotoMedium : AppCompatEditText {
 
--- app/src/main/java/id/pahlevikun/easygroupmaker/view/ui/MainActivity.kt
+++ app/src/main/java/id/pahlevikun/easygroupmaker/view/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import id.pahlevikun.easygroupmaker.R
-import id.pahlevikun.easygroupmaker.composer.util.ArrayConversion
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.util.*
